2da9bec42f247c071e5378a560244b63c74ba448
sfkrnwi <sfkrnwi@students.cs.ubc.ca>
2024-01-29 19:32:04 -0800

    commented out their code

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -22,110 +22,110 @@ export interface ITestQuery {
 	expected: any;
 }
 
-describe("InsightFacade", function () {
-	let facade: IInsightFacade;
+// describe("InsightFacade", function () {
+// 	let facade: IInsightFacade;
 
-	// Declare datasets used in tests. You should add more datasets like this!
-	let sections: string;
+// 	// Declare datasets used in tests. You should add more datasets like this!
+// 	let sections: string;
 
-	before(async function () {
-		// This block runs once and loads the datasets.
-		sections = await getContentFromArchives("pair.zip");
+// 	before(async function () {
+// 		// This block runs once and loads the datasets.
+// 		sections = await getContentFromArchives("pair.zip");
 
-		// Just in case there is anything hanging around from a previous run of the test suite
-		await clearDisk();
-	});
+// 		// Just in case there is anything hanging around from a previous run of the test suite
+// 		await clearDisk();
+// 	});
 
-	describe("AddDataset", function () {
+// 	describe("AddDataset", function () {
 
-		beforeEach(function () {
-			// This section resets the insightFacade instance
-			// This runs before each test
-			facade = new InsightFacade();
-		});
+// 		beforeEach(function () {
+// 			// This section resets the insightFacade instance
+// 			// This runs before each test
+// 			facade = new InsightFacade();
+// 		});
 
-		afterEach(async function () {
-			// This section resets the data directory (removing any cached data)
-			// This runs after each test, which should make each test independent of the previous one
-			await clearDisk();
-		});
+// 		afterEach(async function () {
+// 			// This section resets the data directory (removing any cached data)
+// 			// This runs after each test, which should make each test independent of the previous one
+// 			await clearDisk();
+// 		});
 
-		it("should reject with  an empty dataset id", async function () {
-			const result = facade.addDataset("", sections, InsightDatasetKind.Sections);
+// 		it("should reject with  an empty dataset id", async function () {
+// 			const result = facade.addDataset("", sections, InsightDatasetKind.Sections);
 
-			return expect(result).to.eventually.be.rejectedWith(InsightError);
-		});
-	});
+// 			return expect(result).to.eventually.be.rejectedWith(InsightError);
+// 		});
+// 	});
 
-	/*
-	 * This test suite dynamically generates tests from the JSON files in test/resources/queries.
-	 * You can and should still make tests the normal way, this is just a convenient tool for a majority of queries.
-	 */
-	describe("PerformQuery", function () {
-		before(async function () {
-			facade = new InsightFacade();
+// 	/*
+// 	 * This test suite dynamically generates tests from the JSON files in test/resources/queries.
+// 	 * You can and should still make tests the normal way, this is just a convenient tool for a majority of queries.
+// 	 */
+// 	describe("PerformQuery", function () {
+// 		before(async function () {
+// 			facade = new InsightFacade();
 
-			// Add the datasets to InsightFacade once.
-			// Will *fail* if there is a problem reading ANY dataset.
-			const loadDatasetPromises = [
-				facade.addDataset("sections", sections, InsightDatasetKind.Sections),
-			];
+// 			// Add the datasets to InsightFacade once.
+// 			// Will *fail* if there is a problem reading ANY dataset.
+// 			const loadDatasetPromises = [
+// 				facade.addDataset("sections", sections, InsightDatasetKind.Sections),
+// 			];
 
-			try {
-				await Promise.all(loadDatasetPromises);
-			} catch(err) {
-				throw new Error(`In PerformQuery Before hook, dataset(s) failed to be added. \n${err}`);
-			}
-		});
+// 			try {
+// 				await Promise.all(loadDatasetPromises);
+// 			} catch(err) {
+// 				throw new Error(`In PerformQuery Before hook, dataset(s) failed to be added. \n${err}`);
+// 			}
+// 		});
 
-		after(async function () {
-			await clearDisk();
-		});
+// 		after(async function () {
+// 			await clearDisk();
+// 		});
 
-		describe("valid queries", function() {
-			let validQueries: ITestQuery[];
-			try {
-				validQueries = readFileQueries("valid");
-			} catch (e: unknown) {
-				expect.fail(`Failed to read one or more test queries. ${e}`);
-			}
+// 		describe("valid queries", function() {
+// 			let validQueries: ITestQuery[];
+// 			try {
+// 				validQueries = readFileQueries("valid");
+// 			} catch (e: unknown) {
+// 				expect.fail(`Failed to read one or more test queries. ${e}`);
+// 			}
 
-			validQueries.forEach(function(test: any) {
-				it(`${test.title}`, function () {
-					return facade.performQuery(test.input).then((result) => {
-						assert.fail("Write your assertions here!");
-					}).catch((err: any) => {
-						assert.fail(`performQuery threw unexpected error: ${err}`);
-					});
-				});
-			});
-		});
+// 			validQueries.forEach(function(test: any) {
+// 				it(`${test.title}`, function () {
+// 					return facade.performQuery(test.input).then((result) => {
+// 						assert.fail("Write your assertions here!");
+// 					}).catch((err: any) => {
+// 						assert.fail(`performQuery threw unexpected error: ${err}`);
+// 					});
+// 				});
+// 			});
+// 		});
 
-		describe("invalid queries", function() {
-			let invalidQueries: ITestQuery[];
+// 		describe("invalid queries", function() {
+// 			let invalidQueries: ITestQuery[];
 
-			try {
-				invalidQueries = readFileQueries("invalid");
-			} catch (e: unknown) {
-				expect.fail(`Failed to read one or more test queries. ${e}`);
-			}
+// 			try {
+// 				invalidQueries = readFileQueries("invalid");
+// 			} catch (e: unknown) {
+// 				expect.fail(`Failed to read one or more test queries. ${e}`);
+// 			}
 
-			invalidQueries.forEach(function(test: any) {
-				it(`${test.title}`, function () {
-					return facade.performQuery(test.input).then((result) => {
-						assert.fail(`performQuery resolved when it should have rejected with ${test.expected}`);
-					}).catch((err: any) => {
-						if (test.expected === "InsightError") {
-							expect(err).to.be.instanceOf(InsightError);
-						} else {
-							assert.fail("Query threw unexpected error");
-						}
-					});
-				});
-			});
-		});
-	});
-});
+// 			invalidQueries.forEach(function(test: any) {
+// 				it(`${test.title}`, function () {
+// 					return facade.performQuery(test.input).then((result) => {
+// 						assert.fail(`performQuery resolved when it should have rejected with ${test.expected}`);
+// 					}).catch((err: any) => {
+// 						if (test.expected === "InsightError") {
+// 							expect(err).to.be.instanceOf(InsightError);
+// 						} else {
+// 							assert.fail("Query threw unexpected error");
+// 						}
+// 					});
+// 				});
+// 			});
+// 		});
+// 	});
+// });
 
 describe("Add Datasets", function() {
 
