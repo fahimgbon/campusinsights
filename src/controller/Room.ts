79635d22877708b0d44bc17d8e6356223b32c3d5
Fahim Gbon <fahimgbon@dhcp-206-12-165-136.ubcsecure.wireless.ubc.ca>
2024-02-28 20:52:02 -0800

    Refactoring InsightFacade to use interface to process Section and Room kinds

---
+++ src/controller/Room.ts
@@ -0,0 +1,131 @@
+export default class Room {
+	private fullname: string; // Full building name
+	private shortname: string; // Short building name
+	private number: string; // The room number. Not always a number so represented as a string
+	private name: string; // The room id. Should be rooms shortname+"_"+rooms number
+	private address: string; // The building address
+	private lat: number; // The latitude of the building
+	private lon: number; // The longitude of the building
+	private seats: number; // The number of seats in the room
+	private type: string; // The room type
+	private furniture: string; // The room furniture
+	private href: string; // The link to the full details online
+
+	constructor(
+		fullname: string,
+		shortname: string,
+		number: string,
+		name: string,
+		seats: number,
+		type: string,
+		furniture: string,
+		address: string,
+		lat: number,
+		lon: number,
+		href: string
+	) {
+		this.fullname = fullname;
+		this.shortname = shortname;
+		this.number = number;
+		this.name = shortname + "_" + number;
+		this.seats = seats;
+		this.type = type;
+		this.furniture = furniture;
+		this.address = address;
+		this.lat = lat;
+		this.lon = lon;
+		this.href = href;
+	}
+
+	// Getters
+	public getFullname(): string {
+		return this.fullname;
+	}
+
+	public getShortname(): string {
+		return this.shortname;
+	}
+
+	public getNumber(): string {
+		return this.number;
+	}
+
+	public getName(): string {
+		return this.name;
+	}
+
+	public getSeats(): number {
+		return this.seats;
+	}
+
+	public getType(): string {
+		return this.type;
+	}
+
+	public getFurniture(): string {
+		return this.furniture;
+	}
+
+	public getAddress(): string {
+		return this.address;
+	}
+
+	public getLat(): number {
+		return this.lat;
+	}
+
+	public getLon(): number {
+		return this.lon;
+	}
+
+	public getHref(): string {
+		return this.href;
+	}
+
+	// Setters
+	public setFullname(fullname: string): void {
+		this.fullname = fullname;
+	}
+
+	public setShortname(shortname: string): void {
+		this.shortname = shortname;
+		this.name = shortname + "_" + this.number; // Updates the name as well
+	}
+
+	public setNumber(number: string): void {
+		this.number = number;
+		this.name = this.shortname + "_" + number; // Updates the name as well
+	}
+
+	public setName(shortname: string, number: string): void {
+		this.number = shortname + "_" + number;
+	}
+
+	public setSeats(seats: number): void {
+		this.seats = seats;
+	}
+
+	public setType(type: string): void {
+		this.type = type;
+	}
+
+	public setFurniture(furniture: string): void {
+		this.furniture = furniture;
+	}
+
+	public setAddress(address: string): void {
+		this.address = address;
+	}
+
+	public setLat(lat: number): void {
+		this.lat = lat;
+	}
+
+	public setLon(lon: number): void {
+		this.lon = lon;
+	}
+
+	public setHref(href: string): void {
+		this.href = href;
+	}
+}
---
+++ src/controller/SectionProcessor.ts
@@ -0,0 +1,122 @@
+import JSZip from "jszip";
+import {KindProcessor} from "./KindProcessor";
+import {InsightDatasetKind, InsightError} from "./IInsightFacade";
+import Dataset from "./Dataset";
+import Section from "./Section";
+import InsightFacade from "./InsightFacade";
+
+
+export default class SectionProcessor implements KindProcessor {
+	private insightFacade: InsightFacade;
+
+	constructor(insightFacade: InsightFacade) {
+		this.insightFacade = insightFacade;
+	}
+
+	public processZipContents(id: string, kind: InsightDatasetKind, zipContents: JSZip): Promise<string[]> {
+		if (kind !== InsightDatasetKind.Sections) {
+			return Promise.reject(new InsightError("Invalid kind"));
+		}
+
+		let folderName = "courses/";
+		// files is of type JSZip.JSZipObject[] - ChatGPT:
+		let files = zipContents.folder(folderName)?.file(/.*/);
+
+		if (!files || files.length === 0) {
+			return Promise.reject(new InsightError("Empty or invalid folder"));
+		}
+
+		return this.parseAndSaveSections(id, files);
+	}
+
+	private async parseAndSaveSections(id: string, files: JSZip.JSZipObject[]): Promise<string[]> {
+		let sectionData: Section[] = [];
+		let parseFilePromises: Array<Promise<void>> = [];
+
+		for (let file of files) {
+			parseFilePromises.push(this.parseFileContent(file, sectionData));
+			// console.log("id:", id);
+			// console.log("file:", file);
+			// console.log("Section Data", sectionData);
+		}
+
+		try {
+			await Promise.all(parseFilePromises);
+
+			if (sectionData.length === 0) {
+				throw new InsightError("No valid section found");
+			}
+
+			let dataset = new Dataset(id, JSON.stringify(sectionData), InsightDatasetKind.Sections);
+			this.insightFacade.datasets.set(id, dataset);
+			this.insightFacade.data.set(id, sectionData);
+
+			let rows = 0;
+			// console.log("Rows init", rows);
+			// console.log("Rows dataset init", dataset.numRows);
+			for (let section of sectionData) {
+				rows++;
+			}
+
+			dataset.numRows = rows;
+			// y
+
+			await this.insightFacade.saveDataset(id, dataset, sectionData); // Await saveDataset() call
+			return Array.from(this.insightFacade.datasets.keys());
+		} catch (error) {
+			throw new InsightError(`Failed to parse and save sections: ${error}`);
+		}
+	}
+
+	private parseFileContent(file: JSZip.JSZipObject, sectionData: Section[]): Promise<void> {
+		return file.async("text")
+			.then((text: string) => {
+				// console.log("Text:", text);
+				let json = JSON.parse(text);
+				if (!("result" in json) || !Array.isArray(json.result)) {
+					throw new InsightError("Invalid file format: 'result' field missing or not an array");
+				}
+				let result = json.result;
+				// console.log("Result:", result);
+
+				// checks every element of result array, ie every section
+				// if (result.length > 0) {
+				for (let section of result) {
+					// console.log("Section:", section);
+					// console.log("Professor1:", section.Professor);
+					if (this.isValidSection(section)) {
+						// console.log("Professor2:", section.Professor);
+						let newSection = new Section(
+							section.id,
+							section.Course,
+							section.Title,
+							section.Professor,
+							section.Subject,
+							section.Year,
+							section.Avg,
+							section.Pass,
+							section.Fail,
+							section.Audit
+						);
+						// console.log("NewSection:", newSection);
+						// Add the section to sectionData
+						sectionData.push(newSection);
+					}
+				}
+				// } else {
+				// 	throw new InsightError("Nothing in the result key"); // !!! Necessary check?
+				// }
+			});
+	}
+
+	private isValidSection(section: any): boolean {
+        // A section is valid if it contains every field which can be used by a query and !!! if they are the correct types (should i check?)
+		let fields = ["id", "Course", "Title", "Professor", "Subject", "Year", "Avg", "Pass", "Fail", "Audit"];
+		for (let field of fields) {
+			if (!(field in section)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
