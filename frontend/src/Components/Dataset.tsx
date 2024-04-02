db2f52addccc03bbf087e758aceb1d1f69657d74
sfkrnwi <sfkrnwi@students.cs.ubc.ca>
2024-04-02 11:40:40 -0700

    Update Dataset.tsx

--- frontend/src/Components/Dataset.tsx
+++ frontend/src/Components/Dataset.tsx
@@ -42,6 +42,17 @@ export const options = {
     },
   };
 
+  export const options3 = {
+    responsive: true,
+    plugins: {
+
+      title: {
+        display: true,
+        text: 'Top 5 Courses With Most Sections',
+      },
+    },
+  };
+
 function Dataset(id) {
 
     const [data, setData] = useState<any>();
@@ -55,7 +66,9 @@ function Dataset(id) {
                 "COLUMNS": [
                   id.id + "_dept",
                   id.id + "_avg",
-                  id.id + "_instructor"
+                  id.id + "_instructor",
+                  id.id + "_title"
+
                 ],
                 "ORDER": id.id + "_avg"
               }
@@ -66,20 +79,23 @@ function Dataset(id) {
               const avgSummed = {}
               const deps: string[] = []
               const profs: string[] = []
+              const titles: string[] = []
+
               response.data.result.forEach(element => {
                 const avgKey = id.id + "_dept";
                 const deptKey = id.id + "_avg";
                 const profKey = id.id + "_instructor";
-
+                const titleKey = id.id + "_title"
                 const dept = element[avgKey];
                 const avg = element[deptKey]
                 const prof = element[profKey];
-
+                const title = element[titleKey];
                 if(!avgSummed[dept]) {
                   avgSummed[dept] = []
                   deps.push(dept)
                 }
                 profs.push(dept)
+                titles.push(title)
                 avgSummed[dept].push(avg)
     
               });
@@ -133,6 +149,33 @@ function Dataset(id) {
                 ],
               };
               setData2(dataToBe2)
+              const count2 = {}
+              titles.forEach(prof => {
+                if (!count2[prof]) {
+                    count2[prof] = 0;
+                }
+                count2[prof] = count2[prof] + 1;
+              })
+              const profArray2 = Object.entries(count2).map(([string,count])  => ({string, count}))
+              profArray2.sort((a ,b) => (b as any).count - (a as any).count)
+              const top5Profs2 = profArray2.slice(0,5);
+              const names2 = top5Profs2.map(item => item.string)
+              const counts2 = top5Profs2.map(item => item.count)
+              const dataToBe3 = {
+                labels: names2,
+                datasets: [
+                  {
+                    label: id.id + " Data",
+                    backgroundColor: 'yellow',
+                    borderColor: 'rgba(255, 99, 132, 0.5)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'yellow',
+                    hoverBorderColor: 'red',
+                    data: counts2,
+                  },
+                ],
+              };
+              setData3(dataToBe3)
             }) 
             
       }, [])
@@ -156,7 +199,7 @@ function Dataset(id) {
         }
         {
            data && <div style={{ width: '370px', height: '200px', paddingRight:30 }}>
-            <Bar data = {data} options={options} ></Bar> 
+            <Bar data = {data3} options={options3} ></Bar> 
             </div>
         }
         </div>
